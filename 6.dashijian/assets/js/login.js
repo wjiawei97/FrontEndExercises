38289e54c7c49e727fc3a0612a90f92e86298d96
wjiawei97 <wjiawei97@gmail.com>
2021-08-11 19:23:14 +0800

    完成了登录和注册功能的开发

--- 6.dashijian/assets/js/login.js
+++ 6.dashijian/assets/js/login.js
@@ -31,6 +31,35 @@ $(function() {
     }
   })
 
+
+  // var form=layui.form
+  // form.verify({
+  //   pwd:[/^\S{6,12}$/,'密码必须6到12位，且不能出现空格'],
+  //   repwd:function(value){
+  //     var pwd=$('.reg-box[name=password]').val()
+  //     if(pwd!==value){
+  //       return '两次密码不一致！'
+  //     }
+  //   }
+  // })
+
+  // var layer=layui.layer
+  // $('#form_reg').on('submit',function(e){
+  //   e.preventDefault()
+  //   var data={
+  //     username:$('#form_reg[name=username]').val(),
+  //     password:$('#form_reg[name=password]').val()
+  //   }
+  //   $.post('http://ajax,frontend.itheima.net/api/reguser',data,function(res){
+  //     if(res.status!==0){
+  //       console.log(res);
+  //       layer.msg(res.message)
+  //     }
+  //     layer.msg('注册成功，请登录！')
+  //     $('#link_login').click();
+  //   })
+  // })
+
   // 监听注册表单的提交事件
   $('#form_reg').on('submit', function(e) {
     // 1. 阻止默认的提交行为
@@ -40,7 +69,7 @@ $(function() {
       username: $('#form_reg [name=username]').val(),
       password: $('#form_reg [name=password]').val()
     }
-    $.post('/api/reguser', data, function(res) {
+    $.post('http://ajax.frontend.itheima.net/api/reguser', data, function(res) {
       if (res.status !== 0) {
         return layer.msg(res.message)
       }
@@ -50,24 +79,42 @@ $(function() {
     })
   })
 
-  // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
-    // 阻止默认提交行为
+//   // 监听登录表单的提交事件
+//   $('#form_login').submit(function(e) {
+//     // 阻止默认提交行为
+//     e.preventDefault()
+//     $.ajax({
+//       url: '/api/login',
+//       method: 'POST',
+//       // 快速获取表单中的数据
+//       data: $(this).serialize(),
+//       success: function(res) {
+//         if (res.status !== 0) {
+//           return layer.msg('登录失败！')
+//         }
+//         layer.msg('登录成功！')
+//         // 将登录成功得到的 token 字符串，保存到 localStorage 中
+//         localStorage.setItem('token', res.token)
+//         // 跳转到后台主页
+//         location.href = '/index.html'
+//       }
+//     })
+//   })
+
+  $('#form_login').submit(function(e){
     e.preventDefault()
     $.ajax({
-      url: '/api/login',
-      method: 'POST',
-      // 快速获取表单中的数据
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layer.msg('登录失败！')
+      url:'/api/login',
+      method:'post',
+      data:$(this).serialize(),
+      success:function(res){
+        if(res.status!==0){
+          return layer.msg('登录失败')
         }
         layer.msg('登录成功！')
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
-        localStorage.setItem('token', res.token)
-        // 跳转到后台主页
-        location.href = '/index.html'
+        console.log(res.token);
+        localStorage.setItem('token',res.token)
+        location.href='/index.html'
       }
     })
   })
